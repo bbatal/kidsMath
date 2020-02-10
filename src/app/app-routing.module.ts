fd057e3bcb79d7cfdc798ead6094c9cf4aee2a86
bbatal <bartbatalinski@gmail.com>
2020-02-10 13:59:08 -0500

    created basic markup and styling/ no logic done just yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdditionComponent } from './addition/addition.component';
+import { AppComponent } from './app.component';
+import { SubtractionComponent } from './subtraction/subtraction.component';
+import { MultiplyComponent } from './multiply/multiply.component';
+import { ComparisonComponent } from './comparison/comparison.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "addition", component: AdditionComponent },
+  { path: "subtraction", component: SubtractionComponent },
+  { path: "multiply", component: MultiplyComponent },
+  { path: "comparison", component: ComparisonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
